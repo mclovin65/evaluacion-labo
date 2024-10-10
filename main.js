ba065283289190aca88ba38e029270f5c560b95a
mclovin65 <158982735+mclovin65@users.noreply.github.com>
2024-10-10 16:44:35 +0000

    componente 1 terminada

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-let dom=document.querySelector("#root");
+import { navBarDiv } from "./componentes/componente1/componente1.js";
+
+
+let DOM = document.querySelector("#root");
+
+
+let navBar = document.createElement('section');
+navBar.className = "nav-bar";
+navBar.appendChild(navBarDiv);
+DOM.appendChild(navBar);
+
+/* proyect */
+let proyect = document.createElement('section');
+proyect.className = "proyect";
+proyect.appendChild (proyectos()); //<------- componente
+DOM.appendChild(proyect);
